b8c467fca255ae7e4bd7c9e199e5cfa73b4dfb6e
Gabriel-Turitto <113439287+Gabriel-Turitto@users.noreply.github.com>
2023-02-01 11:40:51 -0500

    Add files via upload

--- Hackathon/Program.cs
+++ Hackathon/Program.cs
@@ -3,55 +3,25 @@ Menu();
 
 choice = Convert.ToInt32(Console.ReadLine());
 ClearScreen();
-while (choice != 5)
+while (choice != 9)
 {
     if (choice == 1)
     {
-        Console.WriteLine("Welcome to the BlueCross blog");
-        Console.WriteLine("----------------------------------------");
-        Console.WriteLine("This section will include a live feed of the BlueCross blog so our consumers and employees can have easy access to \nBlueCross news.\n");
-
+        Console.WriteLine("This will display your member ID card");
         Menu();
         choice = Convert.ToInt32(Console.ReadLine());
         ClearScreen();
     }
     else if (choice == 2)
     {
-        Console.WriteLine("What kind of question would you like to ask?");
-        Console.WriteLine("1. Generic question");
-        Console.WriteLine("2. Generic question");
-        Console.WriteLine("3. Contact a professional directly via sms");
-
-        int choice2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("This will display any upcoming appoin and prescription pickups");
+        Menu();
+        choice = Convert.ToInt32(Console.ReadLine());
         ClearScreen();
-
-        if (choice2 == 1 || choice2 == 2)
-        {
-            Console.WriteLine("Generic answer");
-            Console.WriteLine("Is there anything else you'd like to ask?\n");
-
-            Menu();
-            choice = Convert.ToInt32(Console.ReadLine());
-            ClearScreen();
-        }
-        else if (choice2 == 3)
-        {
-            Console.WriteLine("Opens a chatbox with a medical professional\n");
-
-            Menu();
-            choice = Convert.ToInt32(Console.ReadLine());
-            ClearScreen();
-        }
-
     }
     else if (choice == 3)
     {
-        Console.WriteLine("Please enter your zipcode");
-        string zipcode = Console.ReadLine();
-
-        Console.WriteLine("Here are some clinics near you:");
-        Console.WriteLine("Clinic 1");
-        Console.WriteLine("Clinic 2\n");
+        Console.WriteLine("This will prompt the user with a search bar");
 
         Menu();
         choice = Convert.ToInt32(Console.ReadLine());
@@ -59,9 +29,50 @@ while (choice != 5)
     }
     else if (choice == 4)
     {
-        Console.WriteLine("Here is your current health coverage: coverage example");
-        Console.WriteLine("Here are other insurance plans we have available: list of insurance plans\n");
-
+        Console.WriteLine("This will display the users current coverage plan and other plans they could be eligible for");
+        Menu();
+        choice = Convert.ToInt32(Console.ReadLine());
+        ClearScreen();
+    }
+    else if (choice == 5)
+    {
+        Console.WriteLine("Through Hello Blue you can do all of the features of the app, but through voice activated software");
+        Menu();
+        choice = Convert.ToInt32(Console.ReadLine());
+        ClearScreen();
+    }
+    else if (choice == 6)
+    {
+        Console.WriteLine("This will prompt the user with a chatbox and some pre-generated generic questions.");
+        Menu();
+        choice = Convert.ToInt32(Console.ReadLine());
+        ClearScreen();
+    }
+    else if (choice == 7)
+    {
+        Console.WriteLine("The user will be put on a call with an available medical professional");
+        Menu();
+        choice = Convert.ToInt32(Console.ReadLine());
+        ClearScreen();
+    }
+    else if (choice == 8)
+    {
+        Console.WriteLine("Enter your zipcode");
+        Console.ReadLine();
+        Console.WriteLine("Clinc 1");
+        Console.WriteLine("Would you like to schedule an appointment?");
+        string choice2 = Console.ReadLine();
+        ClearScreen();
+        if(choice2 == "yes" || choice2 == "Yes" )
+        {
+            Console.WriteLine("Appointment scheduled");
+        }
+        else
+        {
+            Menu();
+            choice = Convert.ToInt32(Console.ReadLine());
+            ClearScreen();
+        }
         Menu();
         choice = Convert.ToInt32(Console.ReadLine());
         ClearScreen();
@@ -70,10 +81,14 @@ while (choice != 5)
 
 static void Menu()
 {
-    Console.WriteLine("1. Access the BlueCross blog");
-    Console.WriteLine("2. Ask a professional");
-    Console.WriteLine("3. Find a clinic near you");
-    Console.WriteLine("4. Check your coverage");
+    Console.WriteLine("1. Member ID");
+    Console.WriteLine("2. Reminders/Alerts");
+    Console.WriteLine("3. Search");
+    Console.WriteLine("4. View coverage plan");
+    Console.WriteLine("5. Hello Blue");
+    Console.WriteLine("6. Ask a professional (SMS)");
+    Console.WriteLine("7. Ask a professional (Phone call)");
+    Console.WriteLine("8. Find a clinic and schedule an appointment");
 }
 
 static void ClearScreen()
